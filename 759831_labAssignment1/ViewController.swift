6b3e093b9c6a1d4e49f925d0f23f2147343b6514
evneet <nancymaan4@gmail.com>
2020-01-14 16:01:18 -0500

    Initial Commit

---
+++ 759831_labAssignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  759831_labAssignment1
+//
+//  Created by Evneet kaur on 2020-01-14.
+//  Copyright © 2020 Evneet kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
